d4e33a79d384b81a43ee18569bf75f8fef8cc3ec
or2209 <orlaharty1@gmail.com>
2020-06-16 11:42:53 +0300

    upload files

---
+++ cmake-build-debug/CMakeFiles/CHECK.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CHECK.exe"
+  "CHECK.exe.manifest"
+  "CHECK.pdb"
+  "CMakeFiles/CHECK.dir/main.cpp.obj"
+  "libCHECK.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CHECK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
